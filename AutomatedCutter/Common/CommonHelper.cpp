248ea9bb8179f32914a3ebf0ea24499a5cd896a2
AnishKrishnan <anish.krishnan2708@gmail.com>
2015-02-21 15:00:41 +1300

    Converted conversion functions to generic functions

--- AutomatedCutter/Common/CommonHelper.cpp
+++ AutomatedCutter/Common/CommonHelper.cpp
@@ -10,53 +10,53 @@
 //CommonHelper::~CommonHelper(void)
 //{
 //}
-
-void CommonHelper::ConvertIntToCharArray(int pInt, char* pIntArray, Logger* pLog)
-{
-	if(pLog == NULL)
-	{
-		throw AutoCutterException("CommonHelper::ConvertIntToCharArray - pLog is null");
-	}
-
-	pLog->Log(std::string("CommonHelper::ConvertItToCharArray - Start"));
-	
-	pLog->Log(std::string("Initialising array and pointer"));
-	
-	char* x = (char*)&pInt;
-
-	pLog->Log(std::string("Creating array"));
-	for(int i = 0; i < sizeof(int); i++)
-	{
-		pIntArray[i] = x[i];
-	}
-	
-	pLog->Log(std::string("CommonHelper::ConvertItToCharArray - Finish"));
-	
-}
-
-int CommonHelper::ConvertCharArrayToInt(char* pCharArray, Logger* pLog)
-{
-	if(pLog == NULL)
-	{
-		throw AutoCutterException("CommonHelper::ConvertCharArrayToInt - pLog is null");
-	}
-
-	pLog->Log(std::string("CommonHelper::ConvertCharArrayToInt - Start"));
-	
-	if(pCharArray == NULL)
-	{
-		throw AutoCutterException("CommonHelper::ConvertCharArrayToInt - pCharArray is null");
-	}
-	
-	int value = 0;
-	char* pointerToInt = (char*)&value;
-	for(int i = 0; i < sizeof(int); i++)
-	{
-		pointerToInt[i] = pCharArray[i];
-	}
-
-	pLog->Log(std::string("Value of char array is %i"), value);
-	pLog->Log(std::string("CommonHelper::ConvertCharArrayToInt - Finish"));
-
-	return value;
-}
+//
+//void CommonHelper::ConvertIntToCharArray(int pInt, char* pIntArray, Logger* pLog)
+//{
+//	if(pLog == NULL)
+//	{
+//		throw AutoCutterException("CommonHelper::ConvertIntToCharArray - pLog is null");
+//	}
+//
+//	pLog->Log(std::string("CommonHelper::ConvertItToCharArray - Start"));
+//	
+//	pLog->Log(std::string("Initialising array and pointer"));
+//	
+//	char* x = (char*)&pInt;
+//
+//	pLog->Log(std::string("Creating array"));
+//	for(int i = 0; i < sizeof(int); i++)
+//	{
+//		pIntArray[i] = x[i];
+//	}
+//	
+//	pLog->Log(std::string("CommonHelper::ConvertItToCharArray - Finish"));
+//	
+//}
+//
+//int CommonHelper::ConvertCharArrayToInt(char* pCharArray, Logger* pLog)
+//{
+//	if(pLog == NULL)
+//	{
+//		throw AutoCutterException("CommonHelper::ConvertCharArrayToInt - pLog is null");
+//	}
+//
+//	pLog->Log(std::string("CommonHelper::ConvertCharArrayToInt - Start"));
+//	
+//	if(pCharArray == NULL)
+//	{
+//		throw AutoCutterException("CommonHelper::ConvertCharArrayToInt - pCharArray is null");
+//	}
+//	
+//	int value = 0;
+//	char* pointerToInt = (char*)&value;
+//	for(int i = 0; i < sizeof(int); i++)
+//	{
+//		pointerToInt[i] = pCharArray[i];
+//	}
+//
+//	pLog->Log(std::string("Value of char array is %i"), value);
+//	pLog->Log(std::string("CommonHelper::ConvertCharArrayToInt - Finish"));
+//
+//	return value;
+//}
