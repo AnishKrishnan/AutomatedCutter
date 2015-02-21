33cadfa34656da573f0ef85888762c5309df93cb
AnishKrishnan <anish.krishnan2708@gmail.com>
2015-02-21 13:46:47 +1300

    Created Comms controller class

---
+++ AutomatedCutter/ManagedComms/CommsController.h
@@ -0,0 +1,20 @@
+#pragma once
+ref class CommsController
+{
+#pragma region Public Methods
+
+public:
+	CommsController(void);
+
+#pragma endregion
+
+private: 
+#pragma region Private Methods
+
+#pragma endregion
+
+#pragma region Private Members
+
+#pragma endregion
+};
+
